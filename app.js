8fb2376d5729e2889bc5252d42733e82eb334671
Ariza-salaman <ydw25423@live.com>
2024-05-04 16:56:00 +0800

    20240504 update

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import multer from 'multer';
+const upload = multer(); // 配置multer，这里使用默认设置
 
 const app = express();
 const PORT = 3000;
@@ -15,9 +17,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // 处理表单提交的路由
-app.post('/submit', (req, res) => {
-  console.log(req.body); // 打印提交的表单数据
-  res.send("表单数据接收成功！");
+app.post('/submit', upload.any(), (req, res) => {
+  const userData = {
+    user_name: req.body.user_name,
+    user_email: req.body.user_email,
+    user_message: req.body.user_message
+  };
+
+  // 打印为格式化的JSON字符串，其中缩进设置为2个空格
+  console.log(JSON.stringify(userData, null, 2));
+  res.send(`hello ${req.body.user_name}, thanks for your submission!`);
 });
 
 // 启动服务器
